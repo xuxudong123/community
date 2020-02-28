a046fdacd77f590ec3a43b7f60e5afdf3492b118
xuxudong <xuxudong@sunline.cn>
2020-02-28 16:57:59 +0800

    init rep

---
+++ src/main/java/com/xudong/community/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.xudong.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by xuxudong on 2020-02-28 15:29
+ */
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
