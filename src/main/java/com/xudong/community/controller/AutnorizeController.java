ec97a9c53a0657d1f0772ef381679fc988b57129
xuxudong123 <18324715069@163.com>
2020-02-29 21:03:38 +0800

    将代码中的动态数据放入配置文件中

--- src/main/java/com/xudong/community/controller/AutnorizeController.java
+++ src/main/java/com/xudong/community/controller/AutnorizeController.java
@@ -5,6 +5,7 @@ import com.xudong.community.dto.AccessTokenDTO;
 import com.xudong.community.dto.GithubUser;
 import com.xudong.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,14 +18,23 @@ public class AutnorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String secret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state") String state){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("Iv1.4074cf876049ffa6");
-        accessTokenDTO.setClient_secret("26dd61365c49b489b7a13b294a149a6680487c13");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(secret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
